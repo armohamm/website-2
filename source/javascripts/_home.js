bd1f1ab8e934a2233ebd24165f81fa7c6eb53e20
Ronald Dijkstra <ronald@defacto.nl>
2017-09-07 10:21:19 +0200

    German event appointment (#803)
    * Add form to event
    
    * Add formkeep URL
    
    * Remove space and use H4

--- source/javascripts/_home.js
+++ source/javascripts/_home.js
@@ -12,7 +12,7 @@ $(function () {
     $form.find('input[name="name"]').focus();
 
     // replace button with text
-    label = label.charAt(0).toLowerCase() + label.slice(1) + ':';
-    $this.replaceWith(label);
+    label = label.charAt(0) + label.slice(1) + ':';
+    $this.replaceWith('<h4>' + label + '</h4>');
   });
 });
