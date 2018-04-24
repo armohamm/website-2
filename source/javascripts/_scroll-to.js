8f8455a11b2176504b0f6c3ac83026b3ad8c1121
Ronald Dijkstra <ronald@defacto.nl>
2018-04-24 13:48:58 +0200

    CAPP Twaalf Landing Page 🇳🇱 (#1151)
    * Add a page
    
    * Add features
    
    * Add introduction
    
    * Update page URL
    
    * Add page-nav
    
    * Fix mobile
    
    * Improve introduction section
    
    * Padding arround logo/heading
    
    * scroll to with .header height as offset
    
    * From in hero and bottom of page
    
    * New copy (wip)
    
    * Use translation for contact-headings
    
    * Update page url
    
    * Fix hero
    
    * More clear page-nav
    
    * Fix introduction
    
    * Use flex basis for card
    
    * Contact = afspraak maken
    
    * Update screenshots + text
    
    * Update form_keep title
    
    * Remove contact from page-nav
    
    * New CAPP announcement on homepage (#1164)

---
+++ source/javascripts/_scroll-to.js
@@ -0,0 +1,32 @@
+$(function() {
+  "use strict";
+
+  var offsetTop = -$(".header").height();
+
+  $("a[data-scroll-to]").on("click", function() {
+    $.scrollTo(this.hash, 400, {
+      offset: offsetTop
+    });
+
+    return false;
+  });
+
+  $("a[data-scroll-to-form]").on("click", function() {
+    var $target = $(this.hash);
+
+    $.scrollTo($target, 400, {
+      offset: offsetTop,
+      onAfter: function() {
+        $target
+          .find("input, textarea, select")
+          .not(
+            "input[type=hidden],input[type=button],input[type=submit],button"
+          )
+          .filter(":enabled:visible:first")
+          .focus();
+      }
+    });
+
+    return false;
+  });
+});
