ea44f0eb4c342ebe404c6b1a82b553cb0c974717
Ronald Dijkstra <ronald@defacto.nl>
2018-08-23 16:03:30 +0200

    Privacy notice (#1301)
    * Register needed files
    
    * Style it
    
    * Make it work
    
    Make it work and change class and file names
    
    * Add German translation

---
+++ source/javascripts/_privacy-notification.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var notification = document.querySelector("[rel='privacy-notification']");
+  var dismissButtons = document.querySelector("[rel='privacy-notification-dismiss']");
+  var cookieName = "privacy_notification_dismissed";
+  var cookieValue = Cookies.get(cookieName);
+
+  console.log(typeof cookieValue);
+  if (cookieValue !== "true") {
+    notification.classList.add("show");
+  }
+
+  dismissButtons.addEventListener("click", closeNotification);
+
+  function closeNotification() {
+    notification.classList.remove("show");
+
+    Cookies.set(cookieName, true, { expires: 365 });
+  }
+});
